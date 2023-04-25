4df36955b11923578d168b9e04c106d4be2c5473
John Guerrero <jpguerrero1022@gmail.com>
2023-04-24 19:18:25 -0500

    resubmitted project files

---
+++ assets/script.js
@@ -0,0 +1,55 @@
+  var local = {};
+  dayjs.locale(local);
+ 
+  $(function () {
+
+    var hour = dayjs().format('H');
+ 
+    function blockColor() {
+      $('.time-block').each(function() {
+        var sectionTime = parseInt(this.id);
+        $(this).toggleClass('past', sectionTime < hour);
+        $(this).toggleClass('present', sectionTime === hour);
+        $(this).toggleClass('future', sectionTime > hour);
+      });
+    }
+
+    function setColor() {
+      $('.time-block').each(function() {
+        var sectionTime = parseInt(this.id);
+        if (sectionTime == hour) {
+          $(this).removeClass('past future').addClass('present');
+        } else if (sectionTime < hour) {
+          $(this).removeClass('future present').addClass('past');
+        } else {
+          $(this).removeClass('past present').addClass('future');
+        }
+      });
+    }
+
+    function updateTime() {
+      var date = $('#date');
+      var currentDate = dayjs().format('dddd, MMMM D, YYYY');
+      date.text(currentDate);
+    }
+
+    function enterText() {
+      $('.saveBtn').on('click', function() {
+        var key = $(this).parent().attr('id');
+        var value = $(this).siblings('.description').val();
+        localStorage.setItem(key, value);
+      });
+    }
+
+    $('.time-block').each(function() {
+      var key = $(this).attr('id');
+      var value = localStorage.getItem(key);
+      $(this).children('.description').val(value);
+    });
+  
+    setColor();
+    blockColor();
+    enterText();                
+  
+    setInterval(updateTime, 1000);
+  });
